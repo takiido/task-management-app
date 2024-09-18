874e9ff0769334823dcd96b72acad1475bf332a5
takiido <157808651+takiido@users.noreply.github.com>
2024-09-18 16:19:26 -0500

    Add project files.

---
+++ task-management-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace task_management_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
